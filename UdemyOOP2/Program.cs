24656ee7e5043d0ebb32c8a5daf672dfbebae2d2
Felipe <lipesmaffini@gmail.com>
2023-11-15 17:39:27 -0400

    Add project files.

---
+++ UdemyOOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using UdemyOOP2;
+
+static int menu() {
+    Console.WriteLine("1 : Valores geometricos de um retangulo");
+    Console.WriteLine("2 : Aumento de salário de Funcionario");
+    Console.WriteLine("3 : Aluno aprovado?");
+    Console.WriteLine("4 : Dolar para real(estático)");
+    Console.Write("\nOpção: ");
+    int op = int.Parse(Console.ReadLine());
+    Console.Clear();
+    return op;
+}
+
+int op = menu();
+switch (op) {
+    case 1:
+        Retangulo retangulo = new Retangulo();
+        Console.WriteLine("Entre a largura e altura do retangulo: ");
+        retangulo.Largura=double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine(' ');
+        retangulo.Altura=double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine($"\nAREA = {retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"PERÍMETRO = {retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"DIAGONAL = {retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        break;
+    case 2:
+
+        break;
+    case 3: 
+        break;
+    case 4:
+        break;
+    default:
+        break;
+}
---
+++ UdemyOOP2/Retangulo.cs
@@ -0,0 +1,16 @@
+﻿namespace UdemyOOP2 {
+    internal class Retangulo {
+        public double Largura {  get; set; }
+        public double Altura {  get; set; }
+
+        public double Area() {
+            return Largura*Altura;
+        }
+        public double Perimetro() {
+            return( (2*+Altura)+(2*Largura));
+        }
+        public double Diagonal() {
+            return (Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2)));
+        }
+    }
+}
